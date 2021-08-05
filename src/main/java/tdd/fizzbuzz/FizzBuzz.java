9cc2353d8cc27c0c24e925f8792a2da0fbfe1496
balajma <mark.lorenz.balajadia@oocl.com>
2021-08-06 00:22:39 +0800

    [TDD][Mark] feat: implement solution when ordernum is multiple of 3, 5 and 7 and AutoFormat

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -3,12 +3,13 @@ package tdd.fizzbuzz;
 public class FizzBuzz {
     public String countOff(int orderNumber) {
 
-        if(orderNumber%5==0 && orderNumber%7==0) return "BuzzWhizz";
-        if(orderNumber%3==0 && orderNumber%5==0) return "FizzBuzz";
-        if(orderNumber%3==0 && orderNumber%7==0) return "FizzWhizz";
-        if(orderNumber%3==0) return "Fizz";
-        if(orderNumber%5==0) return "Buzz";
-        if(orderNumber%7==0) return "Whizz";
+        if (orderNumber % 3 == 0 && orderNumber % 5 == 0 && orderNumber % 7 == 0) return "FizzBuzzWhizz";
+        if (orderNumber % 5 == 0 && orderNumber % 7 == 0) return "BuzzWhizz";
+        if (orderNumber % 3 == 0 && orderNumber % 5 == 0) return "FizzBuzz";
+        if (orderNumber % 3 == 0 && orderNumber % 7 == 0) return "FizzWhizz";
+        if (orderNumber % 3 == 0) return "Fizz";
+        if (orderNumber % 5 == 0) return "Buzz";
+        if (orderNumber % 7 == 0) return "Whizz";
 
         return String.valueOf(orderNumber);
     }
