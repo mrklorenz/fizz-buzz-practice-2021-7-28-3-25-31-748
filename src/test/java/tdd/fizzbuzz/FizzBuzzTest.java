6096470f7d549fb5520f723f273df521ac6be9fe
balajma <mark.lorenz.balajadia@oocl.com>
2021-08-05 13:49:56 +0800

    [TDD][Mark] test: created given_order_number_when_normal_case_then_return_order_number() in FizzBuzzTest.java

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,18 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class FizzBuzzTest {
+
+    @Test
+    public void given_order_number_when_normal_case_then_return_order_number(){
+        int orderNumber = 1;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        String expectedResult = fizzBuzz.countOff(orderNumber);
+
+        assertThat(expectedResult).isEqualTo("1");
+    }
 }
