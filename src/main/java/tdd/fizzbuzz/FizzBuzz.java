e84e0787173c995b1fa05f68d768ee759a84a35d
balajma <mark.lorenz.balajadia@oocl.com>
2021-08-05 14:22:31 +0800

    [TDD][Mark] feat: implement solution when ordernum is multiple of 7 in Fizzbuzz.java

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -5,6 +5,7 @@ public class FizzBuzz {
 
         if(orderNumber%3==0) return "Fizz";
         if(orderNumber%5==0) return "Buzz";
+        if(orderNumber%7==0) return "Whizz";
 
         return String.valueOf(orderNumber);
     }
