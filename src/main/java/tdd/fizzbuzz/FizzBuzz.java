55e2c92c895fc54f78b5a1aebd9ac6d490006ab2
balajma <mark.lorenz.balajadia@oocl.com>
2021-08-06 00:11:46 +0800

    [TDD][Mark] feat: implement solution when ordernum is multiple of 3 and 7 in FizzBuzz.java

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -4,6 +4,7 @@ public class FizzBuzz {
     public String countOff(int orderNumber) {
 
         if(orderNumber%3==0 && orderNumber%5==0) return "FizzBuzz";
+        if(orderNumber%3==0 && orderNumber%7==0) return "FizzWhizz";
         if(orderNumber%3==0) return "Fizz";
         if(orderNumber%5==0) return "Buzz";
         if(orderNumber%7==0) return "Whizz";
