ed368fa0e6035912400f6b70fe5cddcb9a7419da
balajma <mark.lorenz.balajadia@oocl.com>
2021-08-05 14:05:51 +0800

    [TDD][Mark] feat: implement solution in countoff for multiple of 5 in FizzBuzz.java

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,7 +2,9 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
     public String countOff(int orderNumber) {
+
         if(orderNumber%3==0) return "Fizz";
+        if(orderNumber%5==0) return "Buzz";
 
         return String.valueOf(orderNumber);
     }
